b181aa38d65a83d2ddb87d9e5e6cf285af4114fd
LOS <2362819691@qq.com>
2022-12-15 14:10:10 +0800

    新增开关led

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
-int main() {
-  
+#include <stm32f10x.h>
+
+int main(void) {
+  // 启动GPIOC的RCC时钟
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+  // 配置LED引脚
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  // 关灯
+  // GPIO_SetBits(GPIOC, GPIO_Pin_13);
+  // 开灯
+  GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+  while(1) {
+    
+  }
 }
